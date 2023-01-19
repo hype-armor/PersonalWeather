188f30d951067122ca278e4e75c4b056b094cb26
Greg Morgan <gamm90@gmail.com>
2023-01-19 18:14:48 +0000

    simplify code

--- UserPreferences.py
+++ UserPreferences.py
@@ -1,25 +1,19 @@
 class UserPreferences:
-    dict = {'Burning': 90, 'warm': 75, 'justright': 72, 'cool': 65, 'toocold': 60}
-    
+    dictoftemps = {'Burning': 90, 'warm': 75, 'justright': 72, 'cool': 65, 'toocold': 60,
+                   'Freezing': 50, 'arctic': 40, 'polar': 30, 'frozen': 20, 'ice': 10, 'frostbite': 0}
+
     def __init__(self):
         print("UserPreferences.__init__")
-    
+
     def getTemp(self, key):
-        return self.dict[key]
+        return self.dictoftemps[key]
 
     def setTemp(self, key, value):
-        self.dict[key] = value
-    
+        self.dictoftemps[key] = value
+
     def checkTemp(self, value):
-        if value >= self.dict["Burning"]:
-            return "Burning"
-        elif value >= self.dict["warm"]:
-            return "Warm"
-        elif value >= self.dict["justright"]:
-            return "Just right"
-        elif value >= self.dict["cool"]:
-            return "Cool"
-        elif value >= self.dict["toocold"]:
-            return "Too cold"
-        else:
-            return "Freezing"
+        # loop through a dictionary and compare the value to the key
+        for key, temp in self.dictoftemps.items():
+            if value >= temp:
+                return key
+        return "Absolute Zero"
